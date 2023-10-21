14b6ec44b2f5f3d3564569cf1d518c969ca13f58
João Figueredo <joaoklvf@gmail.com>
2023-10-21 11:38:50 -0300

    angular setup

---
+++ api.ts
@@ -0,0 +1,6 @@
+// Substitua o CHAVE_DA_API com a chave gerada no site.
+
+export const API_KEY = 'api_key=dda31cfe5bf9eb6ef49ab1d2aad0d456';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
