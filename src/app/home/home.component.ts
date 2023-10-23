6885ff499cb38c859172a1f06da737ded10ea37d
João Figueredo <joaoklvf@gmail.com>
2023-10-23 08:33:24 -0300

    feat: using loading component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,12 +17,15 @@ export class HomeComponent {
   constructor(private movieService: MovieService) { }
   movie: MovieData | null = null;
   isMovieRequested: boolean = false;
+  isFetching: boolean = false;
 
   getRandomMovie() {
+    this.isFetching = true;
     this.movieService.getRandomMovie().subscribe(movieResponse => {
       this.isMovieRequested = true;
       this.movie = movieResponse?.id ?
         movieResponse : null;
+      this.isFetching = false;
     })
   }
 }
