e8b455dc7d03c64d3f818dc62e5ab29f5c7a7893
João Figueredo <joaoklvf@gmail.com>
2023-10-22 22:46:38 -0300

    feat: home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { MovieService } from '../services/movie.service';
+import { MovieData } from 'src/interfaces/movie';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  constructor(private movieService: MovieService) { }
+  movie: MovieData | null = null;
+  isMovieRequested: boolean = false;
+
+  getRandomMovie() {
+    this.movieService.getRandomMovie().subscribe(movieResponse => {
+      this.isMovieRequested = true;
+      this.movie = movieResponse?.id ? movieResponse : null;
+    })
+  }
 }
